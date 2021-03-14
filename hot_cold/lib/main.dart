e4b4dfbb78f18878db5d70352169996afb4604d4
Anti-Alpha <nikita.lebedintsev@yandex.ru>
2021-03-14 15:41:17 +0200

    Added icons and button row

--- hot_cold/lib/main.dart
+++ hot_cold/lib/main.dart
@@ -5,10 +5,24 @@ import 'package:country_code_picker/country_code_picker.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => new MyAppState();
+}
+
+class MyAppState extends State<MyApp>{
   var countt;
+  var season;
+  Color colorwin = Color(0xff2D3039);
+  Color colorspr = Color(0xff2D3039);
+  Color colorsum = Color(0xff2D3039);
+  Color coloraut = Color(0xff2D3039);
+
+
+  bool win = false, spr = false, sum = false, aut = false;
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: '1',
       home: Scaffold(
@@ -23,8 +37,8 @@ class MyApp extends StatelessWidget {
           ),
         ),
 
-        body: SingleChildScrollView (
-   child: Container (
+        body:
+   Container (
     child: Center(
       child: Column (
         children: <Widget> [
@@ -63,40 +77,202 @@ class MyApp extends StatelessWidget {
             // color: Colors.yellow[600],
           ),
       Row (
-        children: <Widget> [
-           Container(
-               width: 90.0,
-               height: 120.0,
-               decoration: BoxDecoration(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
 
-                 color: Color(0xff2D3039),
+        children: <Widget> [
+
+           Container(
+               width: 80.0,
+               height: 110.0,
+               decoration: BoxDecoration(
+                 color: colorwin,
                  borderRadius: BorderRadius.all(Radius.circular(15.0)),
                ),
               child: TextButton(
-
                   onPressed: () {
-                    print("FlatButton tapped");
+                    setState(() {
+                      colorwin = Color(0xff585D6A);
+                      colorspr = Color(0xff2D3039);
+                      colorsum = Color(0xff2D3039);
+                      coloraut = Color(0xff2D3039);
+                      win = true;
+                      spr = false;
+                      sum = false;
+                      aut = false;
+                      season = 'winter';
+                    });
                   },
 
                   child: Column (
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget> [
                       Container(
                         height: 10,
                         width: 20,
                       ),
                    Image.asset('assets/images/dd.png',
-                    width: 60.0,
-                    height: 60.0,),
+                    width: 40.0,
+                    height: 40.0,),
+                      Container(
+                        height: 10,
+                        width: 20,
+                      ),
+          Text(
+            'Winter',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xffAEB0B3)),
+          ),
+
+
+        ],
+
+      ))),
+          Container(
+              width: 80.0,
+              height: 110.0,
+              decoration: BoxDecoration(
+
+                color: colorspr,
+                borderRadius: BorderRadius.all(Radius.circular(15.0)),
+              ),
+              child: TextButton(
+
+                  onPressed: () {
+                    setState(() {
+                      colorwin = Color(0xff2D3039);
+                      colorspr = Color(0xff585D6A);
+                      colorsum = Color(0xff2D3039);
+                      coloraut = Color(0xff2D3039);
+                      win = false;
+                      spr = true;
+                      sum = false;
+                      aut = false;
+                      season = 'spring';
+                    });
+                  },
+
+                  child: Column (
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget> [
+                      Container(
+                        height: 5,
+                        width: 20,
+                      ),
+                      Image.asset('assets/images/ff.png',
+                        width: 40.0,
+                        height: 40.0,),
                       Container(
                         height: 10,
                         width: 20,
                       ),
                       Text(
-                        'Winter',
+                        'Spring',
                         style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xffAEB0B3)),
-                      )
+                      ),
+
+
                     ],
-      )))
+
+                  ))),
+          Container(
+              width: 80.0,
+              height: 110.0,
+              decoration: BoxDecoration(
+
+                color: colorsum,
+                borderRadius: BorderRadius.all(Radius.circular(15.0)),
+              ),
+              child: TextButton(
+
+                  onPressed: () {
+                    setState(() {
+                      colorwin = Color(0xff2D3039);
+                      colorspr = Color(0xff2D3039);
+                      colorsum = Color(0xff585D6A);
+                      coloraut = Color(0xff2D3039);
+                      win = false;
+                      spr = false;
+                      sum = true;
+                      aut = false;
+                      season = 'summer';
+                    });
+                  },
+
+                  child: Column (
+
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget> [
+                      Container(
+                        height: 5,
+                        width: 20,
+                      ),
+                      Image.asset('assets/images/cc.png',
+                        width: 40.0,
+                        height: 40.0,),
+                      Container(
+                        height: 10,
+                        width: 20,
+                      ),
+                      Text(
+                        'Summer',
+                        style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xffAEB0B3)),
+                      ),
+
+
+                    ],
+
+                  ))),
+          Container(
+              width: 80.0,
+              height: 110.0,
+              decoration: BoxDecoration(
+
+                color: coloraut,
+                borderRadius: BorderRadius.all(Radius.circular(15.0)),
+              ),
+              child: TextButton(
+
+                  onPressed: () {
+                    setState(() {
+                      colorwin = Color(0xff2D3039);
+                      colorspr = Color(0xff2D3039);
+                      colorsum = Color(0xff2D3039);
+                      coloraut = Color(0xff585D6A);
+                      win = false;
+                      spr = false;
+                      sum = false;
+                      aut = true;
+                      season = 'autmn';
+                    });
+                  },
+
+                  child: Column (
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget> [
+                      Container(
+                        height: 5,
+                        width: 20,
+                      ),
+                      Image.asset('assets/images/gg.png',
+                        width: 40.0,
+                        height: 40.0,),
+                      Container(
+                        height: 10,
+                        width: 20,
+                      ),
+                      Text(
+                        'Autmn',
+                        style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xffAEB0B3)),
+                      ),
+
+
+                    ],
+
+                  ))),
         ],
       )
 
@@ -106,7 +282,7 @@ class MyApp extends StatelessWidget {
 
         ),
 
-        ),
+
       ),
     );
   }
